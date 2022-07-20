ea1dac078828515dbf9fb467e2b685811982892e
Mike <mgoldobin@psu.ru>
2022-07-21 02:44:12 +0500

    add anything collectors

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,5 +1,7 @@
 import * as UserActionCreators from './user'
+import * as ThemeActionCreators from './theme'
 
 export const ActionCreators = {
-	...UserActionCreators
+	...UserActionCreators,
+	...ThemeActionCreators
 }
