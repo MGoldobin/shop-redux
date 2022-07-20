00d9eaef5676168158f7ad98844b681a5205bfbf
Mike <mgoldobin@psu.ru>
2022-07-21 02:48:15 +0500

    using collector

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,4 +1,4 @@
-import { UserAction, UserState, UserActionTypes } from '../../types/user'
+import { UserAction, UserState, UserActionTypes } from '../../types/index'
 
 const initialState: UserState = {
 	users: [],
