e71f1fc9b8de26638474230bf642fa2aa7beb588
Mike <mgoldobin@psu.ru>
2022-07-24 00:54:49 +0500

    combine new pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,5 @@
 export * from './MainPage/MainPage'
+export * from './CartPage/CartPage'
+export * from './ProductPage/ProductPage'
+export * from './Layout/Layout'
+export * from './PageNotFound/PageNotFound'
