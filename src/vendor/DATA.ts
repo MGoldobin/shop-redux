0eb932849e690236619614fa64c1c2785f83a910
Mike <mgoldobin@psu.ru>
2022-07-25 03:05:03 +0500

    add test item

--- src/vendor/DATA.ts
+++ src/vendor/DATA.ts
@@ -21,6 +21,15 @@ export const DATA: {status: number, data: Product[]} = {
 			image: 'https://avatars.mds.yandex.net/get-mpic/4011308/img_id4324657931159313606.jpeg/orig',
 			rating: [5, 919]
 		},
+		{
+			id: 3,
+			title: 'HTC Wildfire E Lite',
+			description: 'Экран	5.45" (1440×720) IPS; Встроенная память	16 ГБ; Оперативная память	2 ГБ; Двойная камера	8 МП, 0.30 МП; Аккумулятор	3000 мА·ч; Процессор; MediaTek Helio A20; SIM-карты	2 (nano SIM); Операционная система	Android 10; Беспроводные интерфейсы	Bluetooth, Wi-Fi; Стандарт связи	3G, 4G LTE; Вес	160 г',
+			price: 7490,
+			category: 'Smartphone',
+			image: 'https://avatars.mds.yandex.net/get-mpic/4011308/img_id4324657931159313606.jpeg/orig',
+			rating: [5, 919]
+		},
 	]
 }
 
