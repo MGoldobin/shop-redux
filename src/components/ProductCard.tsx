f0159d7fce15aa3beb363e880178e1f038cc1056
Mike <mgoldobin@psu.ru>
2022-07-25 03:03:53 +0500

    styling product card

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { Product } from '../types/index'
@@ -8,27 +9,99 @@ interface ProductCardProps {
 }
 
 const StyledProductCard = styled.div`
-	color: ${props => props.theme.color};
-  margin: 0 auto;
-  border-radius: 7px 7px 7px 7px;
-	border: 1px solid ${props => props.theme.borderColor};
-	width: 250px;
+	display: flex;
+	align-items: center;
+	margin: 0px auto 0px;
+	background: #fff;
+	box-shadow: 0 3px 6px rgba(0,0,0,0.15), 0 3px 6px rgba(0,0,0,0.15);
 	height: 350px;
+	border-radius: 7px;
+	width: 436px;
 `
 
 const Image = styled.img`
-	width: 100%;
-	height: 300px;
-  border-radius: 7px 7px 0 0;
+	height: 250px;
+	width: 200px;
 	object-fit: contain;
 	background-color: #fff;
+	margin: 0 10px;
+`
+
+const Info = styled.div`
+	display: flex;
+	flex-direction: column;
+	padding: 0 10px;
+  color: #777;
+	background-color: #f7f5f6;
+	height: 100%;
+	flex: 1;
+	border-radius: 0 7px 7px 0;
+`
+
+const Category = styled.h4`
+	margin: 10px 0;
+	font-size: 12pt;
+	text-transform: uppercase;
+	font-family: "Montserrat";
+	font-weight: 300;
+	color: #FF9800;
+`
+
+const Title = styled(NavLink)`
+	font-size: 20pt;
+	margin: 0;
+	font-weight: 500;
+	color: #333;
+	text-decoration: none;
+`
+
+const TechList = styled.ul`
+	padding: 0 0 0 20px;
+	flex: 1;
+`
+
+const Price = styled.p`
+	margin: 0;
+	font-size: 20px;
+	color: #000;
+	display: inline-flex;
+`
+
+const BuyButton = styled.button`
+	background-color: #ffa501;
+	padding: 10px 20px;
+	color: #fff;
+	border-radius: 30px;
+	text-transform: uppercase;
+	font-weight: bold;
+	transition: all .6s;
+	border: 3px solid #ffa501;
+	cursor: pointer;
+	width: 100px;
+	margin: 15px auto 5px;
+
+	&:hover {
+		color: #ffa501;
+		background-color: #fff;
+	}
 `
 
 export const ProductCard: React.FC<ProductCardProps> = ({product}) => {
 	return (
 		<StyledProductCard>
-			<Image src={product.image} alt={product.title}/>
-			{product.title}
-  	</StyledProductCard>
+				<Image src={product.image} />
+				<Info>
+					<Category>{product.category}</Category>
+					<Title to={`product/${product.id}`}>{product.title}</Title>
+					<TechList>
+						{
+							product.description.split(';').slice(0, 3).map(char => <li>{char}</li>)
+						}
+						<NavLink to={`product/${product.id}`}>Подробнее...</NavLink>
+					</TechList>
+					<Price>{product.price} руб</Price>
+					<BuyButton>Купить</BuyButton>
+				</Info>
+		</StyledProductCard>
 	)
 }
