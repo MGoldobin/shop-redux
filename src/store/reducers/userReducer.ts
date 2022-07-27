9fd57f3befce982c58734471dbf0043d9cac924d
Mike <mgoldobin@psu.ru>
2022-07-28 00:55:56 +0500

    create user reducer

---
+++ src/store/reducers/userReducer.ts
@@ -0,0 +1,19 @@
+import { UserState, UserAction, UserActionTypes } from '../../types/index'
+
+const initialState: UserState = {
+	isLogin: false,
+	uid: '',
+	name: '',
+	email: ''
+}
+
+export const userReducer = (state = initialState, action: UserAction): UserState => {
+	switch(action.type) {
+		case UserActionTypes.LOG_IN:
+			return {isLogin: true, uid: action.payload.uid, name: action.payload.name, email: action.payload.email}
+		case UserActionTypes.LOG_OUT:
+			return {isLogin: false, uid: '', name: '', email: ''}
+		default:
+			return state
+	}
+}
