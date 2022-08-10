85b296774c0ba81a760b3182b20fdd47bec29089
Mike <mgoldobin@psu.ru>
2022-08-10 14:58:51 +0500

    add cookie funcs

---
+++ src/utils/constants/cookie.ts
@@ -0,0 +1,4 @@
+export const COOKIE_PREFIX = 'shop'
+export const AUTH_COOKIE = `${COOKIE_PREFIX}/auth-token`
+export const AUTH_USER_NAME_COOKIE = `${COOKIE_PREFIX}/auth-user-name`
+export const AUTH_USER_EMAIL_COOKIE = `${COOKIE_PREFIX}/auth-user-email`
