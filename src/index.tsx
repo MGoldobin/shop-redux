8e821eb32f4a74c25e1bbfa24a0ea9847ce1755a
Mike <mgoldobin@psu.ru>
2022-08-10 14:41:37 +0500

    store provider rendered separately

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux' 
 
 import App from './App'
 import './assets/css/global.css'
 
+import { store } from './store/index'
+
 const container = document.getElementById('root') as HTMLElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+)
