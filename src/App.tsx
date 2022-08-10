097cb0796ee27b3c6bb534f511bae5007a068119
Mike <mgoldobin@psu.ru>
2022-08-10 14:38:25 +0500

    routes are divided into private and open

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Provider } from 'react-redux' 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import { Layout, MainPage, CartPage, ProductPage, PageNotFound, AuthPage } from './pages/index'
 import { CustomThemeProvider } from './components/index'
 
-import { store } from './store/index'
+import { useAuth } from './utils/hooks/useAuth'
+
+export const NotAuthApp = () => (
+	<>
+		<Route path='Auth' element={<AuthPage />} />
+	</>
+)
+
+export const AuthApp = () => (
+	<>
+		<Route path='Cart' element={<CartPage />}/>
+		<Route path='Profile' element={<CartPage />}/>
+	</>
+)
 
 const App: React.FC = () => {
+	const { isLogin } = useAuth()
+	
 	return (
-		<Provider store={store}>
-			<CustomThemeProvider>
-				<BrowserRouter>
-						<Routes>
-							<Route path='Auth' element={<AuthPage />} />
-							<Route path='/' element={<Layout />}>
-								<Route index element={<MainPage />}/>
-								<Route path='Cart' element={<CartPage />}/>
-								<Route path='Product/:productId' element={<ProductPage />} />
-								<Route path='*' element={<PageNotFound />}/>
-							</Route>
-						</Routes>
-				</BrowserRouter>
-			</CustomThemeProvider>
-		</Provider>
+		<CustomThemeProvider>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<Layout />}>
+						<Route index element={<MainPage />}/>
+						<Route path='Product/:productId' element={<ProductPage />} />
+						<Route path='*' element={<PageNotFound />}/>
+						{isLogin && AuthApp() /* я сделяль */} 
+						{!isLogin && NotAuthApp()}
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</CustomThemeProvider>
 	)
 }
 
