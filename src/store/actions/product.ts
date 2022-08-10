d872fa94a80b2af8db6077cb707456c29eff72a5
Mike <mgoldobin@psu.ru>
2022-08-10 14:58:00 +0500

    linted

--- src/store/actions/product.ts
+++ src/store/actions/product.ts
@@ -1,22 +1,22 @@
-import { Product, ProductAction, ProductActionTypes } from "../../utils/types/product"
-import { Dispatch } from "redux"
-import axios from "axios"
+import { Dispatch } from 'redux'
+import axios from 'axios'
 
+import { ProductAction, ProductActionTypes } from '../../utils/types/index'
 import { fetchFakeData } from '../../utils/helpers/DATA'
 
 export const fetchProducts = () => {
-    return async (dispatch: Dispatch<ProductAction>) => {
-        try {
-            dispatch({type: ProductActionTypes.FETCH_PRODUCTS})
-            const prom = await fetchFakeData() //axios.get('https://fakestoreapi.com/products')
-            setTimeout(() => {
-                dispatch({type: ProductActionTypes.FETCH_PRODUCTS_SUCCESS, payload: prom.data})
-            }, 500)
-        } catch (e) {
-            dispatch({
-                type: ProductActionTypes.FETCH_PRODUCTS_ERROR,
-                payload: 'Произошла ошибка при загрузке пользователей'
-            })
-        }
-    }
+	return async (dispatch: Dispatch<ProductAction>) => {
+		try {
+			dispatch({type: ProductActionTypes.FETCH_PRODUCTS})
+			const prom = await fetchFakeData() //axios.get('https://fakestoreapi.com/products')
+			setTimeout(() => {
+				dispatch({type: ProductActionTypes.FETCH_PRODUCTS_SUCCESS, payload: prom.data})
+			}, 500)
+		} catch (e) {
+			dispatch({
+				type: ProductActionTypes.FETCH_PRODUCTS_ERROR,
+				payload: 'Произошла ошибка при загрузке пользователей'
+			})
+		}
+	}
 }
