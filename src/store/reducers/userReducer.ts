d456f8d9786007f84731de35fb35ba8225a3d54c
Mike <mgoldobin@psu.ru>
2022-08-10 15:02:02 +0500

    user reducer

--- src/store/reducers/userReducer.ts
+++ src/store/reducers/userReducer.ts
@@ -1,18 +1,19 @@
+import { AUTH_COOKIE, AUTH_USER_NAME_COOKIE } from '../../utils/constants/cookie'
+import { getCookie } from '../../utils/helpers/cookie'
 import { UserState, UserAction, UserActionTypes } from '../../utils/types/index'
 
 const initialState: UserState = {
-	isLogin: false,
-	uid: '',
-	name: '',
-	email: ''
+	uid: getCookie(AUTH_COOKIE) || null,
+	name: getCookie(AUTH_USER_NAME_COOKIE) || null,
+	email: getCookie(AUTH_USER_NAME_COOKIE) || null
 }
 
 export const userReducer = (state = initialState, action: UserAction): UserState => {
 	switch(action.type) {
 		case UserActionTypes.LOG_IN:
-			return {isLogin: true, uid: action.payload.uid, name: action.payload.name, email: action.payload.email}
+			return {uid: action.payload.uid, name: action.payload.name, email: action.payload.email}
 		case UserActionTypes.LOG_OUT:
-			return {isLogin: false, uid: '', name: '', email: ''}
+			return {uid: null, name: null, email: null}
 		default:
 			return state
 	}
