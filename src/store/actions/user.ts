7cc9f15fdbc20e4e6594b490e5d72fbae92067f4
Mike <mgoldobin@psu.ru>
2022-08-10 15:01:06 +0500

    login and register functions

---
+++ src/store/actions/user.ts
@@ -0,0 +1,68 @@
+import { Dispatch } from 'redux'
+import { FirebaseError } from '@firebase/util'
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+
+import { SignInFormValues, SignUpFormValues } from '../../utils/types/index'
+import { auth } from '../../utils/firebase/index'
+import { UserActionTypes, UserAction } from '../../utils/types/index'
+import { setCookie } from '../../utils/helpers/cookie'
+import { AUTH_COOKIE, AUTH_USER_EMAIL_COOKIE, AUTH_USER_NAME_COOKIE } from '../../utils/constants/cookie'
+
+export const SignInWithEmailAndPassword = (userForm: SignInFormValues, setErrorSignIn: React.Dispatch<React.SetStateAction<string | null>>) => {
+  return async (dispatch: Dispatch<UserAction>) => {
+    try {
+      const { user } = await signInWithEmailAndPassword(auth, userForm.email, userForm.password)
+			console.log(user)
+			setCookie(AUTH_COOKIE, user.uid)
+			setCookie(AUTH_USER_NAME_COOKIE, user.displayName)
+			setCookie(AUTH_USER_EMAIL_COOKIE, user.email)
+      dispatch({
+				type: UserActionTypes.LOG_IN,
+				payload: {
+					uid: await user.getIdToken(),
+					name: user.displayName || user.email,
+					email: user.email
+				}
+			})
+		} catch (error) {
+			if (error instanceof FirebaseError) {
+				setErrorSignIn(error.code)
+				console.log(error.code)
+			}
+		}
+	}
+}
+
+export const SignUpWithEmailAndPassword = (userForm: SignUpFormValues, setErrorSignUp: React.Dispatch<React.SetStateAction<string | null>>) => {
+  return async (dispatch: Dispatch<UserAction>) => {
+    try {
+      const { user } = await createUserWithEmailAndPassword(auth, userForm.email, userForm.password)
+			updateProfile(user, {
+				displayName: userForm.name
+			})
+					/*await addDoc(collection(db, 'users'), {
+					uid: response.user.uid,
+					...user,
+					authProvider: 'local'
+					})
+					*/
+			console.log(user)
+			setCookie(AUTH_COOKIE, user.uid)
+			setCookie(AUTH_USER_NAME_COOKIE, user.displayName)
+			setCookie(AUTH_USER_EMAIL_COOKIE, user.email)
+			dispatch({
+				type: UserActionTypes.LOG_IN,
+				payload: {
+					uid: await user.getIdToken(),
+					name: user.displayName,
+					email: user.email
+				}
+			})
+		} catch (error) {
+			if (error instanceof FirebaseError) {
+				setErrorSignUp(error.code)
+				console.log(error.code)
+			}
+		}
+	}
+}
